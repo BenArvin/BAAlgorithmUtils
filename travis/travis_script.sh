cf43a95fe51bf30d26698fb4e26b0328c55896f7
BenArvin <>
2018-12-20 19:49:42 +0800

    feat: edit travis setting

--- travis/travis_script.sh
+++ travis/travis_script.sh
@@ -4,5 +4,9 @@
 sudo rm -rf /build/*
 sudo rm -rf /dist/*
 
+#edit version
+newv=$(git describe --exact-match --tags)
+sudo sed -i 's/version="[0-9\.]*"/version="'$newv'"/g' setup.py
+
 #build new packages
 sudo python3 setup.py sdist bdist_wheel
